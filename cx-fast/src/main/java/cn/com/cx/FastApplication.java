c0129af1d1384e8be0de42f6f2f74e8a2c047758
夏冬冬 <xiadongdong@git.com.cn>
2018-03-24 14:40:24 +0800

    项目初始化-init

---
+++ cx-fast/src/main/java/cn/com/cx/FastApplication.java
@@ -0,0 +1,17 @@
+package cn.com.cx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xdd
+ * App启动入口
+ */
+@SpringBootApplication
+public class FastApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(FastApplication.class, args);
+	}
+
+}
