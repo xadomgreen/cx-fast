78ecebcc02d07afa82f96a767a5c078fd3176762
夏冬冬 <xiadongdong@git.com.cn>
2018-03-24 15:42:40 +0800

    1 增加注册中心cx-eureka-server 2 增加服务网关cx-gateway

--- cx_gateway/src/main/java/cn/com/cx/gateway/GateWayApplication.java
+++ cx_gateway/src/main/java/cn/com/cx/gateway/GateWayApplication.java
@@ -1,19 +1,16 @@
-package cn.com.cx;
+package cn.com.cx.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-/**
- * @author xdd
- * App启动入口
- */
-@SpringBootApplication
+@EnableZuulProxy // 开启路由转发和过滤功能
 @EnableEurekaClient // 注册到注册中心
-public class FastApplication {
+@SpringBootApplication // 标记springboot程序
+public class GateWayApplication {
 
 	public static void main(String[] args) throws Exception {
-		SpringApplication.run(FastApplication.class, args);
+		SpringApplication.run(GateWayApplication.class, args);
 	}
-
 }
